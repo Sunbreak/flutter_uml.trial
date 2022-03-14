5b878cd557560b5b19b0f91288a680a34818f7e2
Sunbreak <sunbreak.wang@gmail.com>
2022-03-14 21:04:18 +0800

    Add SingleChildRenderObjectWidget & MultiChildRenderObjectWidget
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

--- lib/src/widgets.dart
+++ lib/src/widgets.dart
@@ -1,1 +1,7 @@
 abstract class Widget {}
+
+abstract class RenderObjectWidget extends Widget {}
+
+abstract class SingleChildRenderObjectWidget extends RenderObjectWidget {}
+
+abstract class MultiChildRenderObjectWidget extends RenderObjectWidget {}
