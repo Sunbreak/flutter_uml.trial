62dcf9bf97ecb270e9d081f20ae653aa01d25b2c
Sunbreak <sunbreak.wang@gmail.com>
2022-03-14 20:20:34 +0800

    Add PageRoute
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,1 +1,11 @@
 abstract class Route<T> {}
+
+abstract class OverlayRoute<T> extends Route<T> {}
+
+abstract class TransitionRoute<T> extends OverlayRoute<T> {}
+
+mixin LocalHistoryRoute<T> on Route<T> {}
+
+abstract class ModalRoute<T> extends TransitionRoute<T> with LocalHistoryRoute<T> {}
+
+abstract class PageRoute<T> extends ModalRoute<T> {}
